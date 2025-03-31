9bced71f1e2fa17e1c15e901e5d0bb04badd9b86
YliesChementel <ylies.chementel@alumni.univ-avignon.fr>
2025-03-31 11:22:48 +0200

    début de construction du plateau

--- SFML/sfml_refacto.cpp
+++ SFML/sfml_refacto.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <vector>
+#include <array>
 
 const float PI = 3.14159265359f;
 
@@ -24,9 +25,8 @@ sf::Vector2f milieu(const sf::Vector2f& p1, const sf::Vector2f& p2) {
     return (p1 + p2) / 2.0f;
 }
 
-sf::Vertex createLine(const sf::Vector2f& start, const sf::Vector2f& end, sf::Color color) {
-    return sf::Vertex(start, color), sf::Vertex(end, color);
-
+std::array<sf::Vertex, 2> createLine(const sf::Vector2f& start, const sf::Vector2f& end, sf::Color color) {
+    return {sf::Vertex(start, color), sf::Vertex(end, color)};
 }
 
 sf::ConvexShape createLosange(const sf::Vector2f& p1, const sf::Vector2f& p2, const sf::Vector2f& p3, const sf::Vector2f& p4, sf::Color fill, sf::Color outline) {
@@ -73,12 +73,14 @@ int main() {
         milieu(points[5], points[4]), milieu(points[1], points[2])
     };
 
-    std::vector<sf::Vertex> lines;
-    for (const auto& m : milieux) lines.push_back(createLine(center, m, sf::Color::Red));
+    std::vector<std::array<sf::Vertex, 2>> lines;
+    for (const auto& m : milieux) {
+        lines.push_back(createLine(center, m, sf::Color::Red));
+    }
 
-    sf::ConvexShape losange = createLosange(center, milieux[1], milieux[5] + 0.25f * (points[1] - milieux[5]), milieux[5], sf::Color::Blue, sf::Color::Green);
-    sf::ConvexShape losange2 = createLosange(milieux[1], milieux[1] + 0.25f * (points[2] - milieux[1]), milieux[5] + 0.5f * (points[1] - milieux[5]), milieux[5] + 0.25f * (points[1] - milieux[5]), sf::Color::Black, sf::Color::Green);
-    sf::ConvexShape losange3 = createLosange(milieux[1] + 0.25f * (points[2] - milieux[1]), milieux[1] + 0.5f * (points[2] - milieux[1]), milieux[5] + 0.75f * (points[1] - milieux[5]), milieux[5] + 0.5f * (points[1] - milieux[5]), sf::Color::Black, sf::Color::Green);
+    sf::ConvexShape losange = createLosange(center, center + 0.25f * (milieux[2] - center), center + 0.25f * (matrice2_5 - center) + 0.25f * (mat5_4_end - mat5_4_start), milieux[5], sf::Color::Blue, sf::Color::Green);
+    //sf::ConvexShape losange2 = createLosange(milieux[1], milieux[1] + 0.25f * (points[2] - milieux[1]), milieux[5] + 0.5f * (points[1] - milieux[5]), milieux[5] + 0.25f * (points[1] - milieux[5]), sf::Color::Black, sf::Color::Green);
+    //sf::ConvexShape losange3 = createLosange(milieux[1] + 0.25f * (points[2] - milieux[1]), milieux[1] + 0.5f * (points[2] - milieux[1]), milieux[5] + 0.75f * (points[1] - milieux[5]), milieux[5] + 0.5f * (points[1] - milieux[5]), sf::Color::Black, sf::Color::Green);
     
     bool losangeFilled = false;
     
@@ -92,13 +94,15 @@ int main() {
                 if (isPointInConvexShape(losange, mousePos)) {
                     losange.setFillColor(losangeFilled ? sf::Color::Blue : sf::Color::Red);
                     losangeFilled = !losangeFilled;
-                }
+               }
             }
         }
 
         window.clear();
         window.draw(hexagon);
-        for (const auto& line : lines) window.draw(&line, 2, sf::Lines);
+        for (const auto& line : lines) {
+            window.draw(line.data(), 2, sf::Lines);
+        }
         window.draw(losange);
         window.draw(losange2);
         window.draw(losange3);
